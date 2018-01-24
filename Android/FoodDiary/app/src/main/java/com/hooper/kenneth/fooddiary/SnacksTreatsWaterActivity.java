b05449c9fbc249f3e3554042d61345fea7685063
Kenneth Hooper <Kenneth.Hooper@version1.com>
2018-01-24 15:47:27 +0000

    making screens

--- Android/FoodDiary/app/src/main/java/com/hooper/kenneth/fooddiary/SnacksTreatsWaterActivity.java
+++ Android/FoodDiary/app/src/main/java/com/hooper/kenneth/fooddiary/SnacksTreatsWaterActivity.java
@@ -1,5 +1,6 @@
 package com.hooper.kenneth.fooddiary;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -7,8 +8,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class SnacksTreatsWaterActivity extends AppCompatActivity {
+
+    private final int treatMaxNumber = 15;
+    private List<TextView> treatList = new ArrayList<>();
+    private HashMap<TextView, Integer> timesTouched = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +28,12 @@ public class SnacksTreatsWaterActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*Button doneButton = findViewById(R.id.FoodDoneButton);
-        doneButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v)
-            {
-                finish();
-            }
-        });*/
+        createAndInitialiseDoneButton();
+
+        initialiseTreatList(treatList);
+        handleTreatTouchListener(treatList);
+
+
     }
 
     @Override
@@ -46,5 +56,67 @@ public class SnacksTreatsWaterActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void createAndInitialiseDoneButton()
+    {
+        Button doneButton = findViewById(R.id.SnacksDoneButton);
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                finish();
+            }
+        });
+    }
+
+    private void initialiseTreatList(List<TextView> treatList)
+    {
+        treatList.add((TextView)findViewById(R.id.treatsText1));
+        treatList.add((TextView)findViewById(R.id.treatsText2));
+        treatList.add((TextView)findViewById(R.id.treatsText3));
+        treatList.add((TextView)findViewById(R.id.treatsText4));
+        treatList.add((TextView)findViewById(R.id.treatsText5));
+        treatList.add((TextView)findViewById(R.id.treatsText6));
+        treatList.add((TextView)findViewById(R.id.treatsText7));
+        treatList.add((TextView)findViewById(R.id.treatsText8));
+        treatList.add((TextView)findViewById(R.id.treatsText9));
+        treatList.add((TextView)findViewById(R.id.treatsText10));
+        treatList.add((TextView)findViewById(R.id.treatsText11));
+        treatList.add((TextView)findViewById(R.id.treatsText12));
+        treatList.add((TextView)findViewById(R.id.treatsText13));
+        treatList.add((TextView)findViewById(R.id.treatsText14));
+        treatList.add((TextView)findViewById(R.id.treatsText15));
+
+        for(int i=0; i< treatMaxNumber; i++)
+        {
+            timesTouched.put(treatList.get(i), 0);
+        }
+    }
+
+    private void handleTreatTouchListener(final List<TextView> treatList)
+    {
+
+    }
+
+    public void treatClickHandler(View target)
+    {
+        for(int i = 0; i < treatMaxNumber; i++) {
+            if (target.getId() == treatList.get(i).getId()) {
+                if(timesTouched.get(treatList.get(i)) %3 == 0) {
+                    treatList.get(i).setBackgroundResource(R.drawable.rounded_corner_red);
+                    timesTouched.put(treatList.get(i), 1);
+                }
+                else if(timesTouched.get(treatList.get(i)) %3 == 1)
+                {
+                    treatList.get(i).setBackgroundResource(R.drawable.rounded_corner_half_red);
+                    timesTouched.put(treatList.get(i), 2);
+                }
+                else if(timesTouched.get(treatList.get(i)) %3 == 2)
+                {
+                    treatList.get(i).setBackgroundResource(R.drawable.rounded_corner_grey);
+                    timesTouched.put(treatList.get(i), 3);
+                }
+            }
+        }
     }
 }
